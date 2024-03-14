4b58ffd25344a530e624a2df0a44badfe80e0f88
Matteo Collina <hello@matteocollina.com>
2024-03-14 18:55:38 +0100

    Be an interceptor
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- README.md
+++ README.md
@@ -8,10 +8,37 @@ An undici dispatcher to in-process Fastify servers
 npm i fastify fastify-undici-dispatcher undici
 ```
 
-## Usage
+## Usage as a Interceptor
 
 ```js
 const { request, Agent } = require('undici')
+const { createFastifyInterceptor } = require('fastify-undici-dispatcher')
+const Fastify = require('fastify')
+const server = Fastify()
+server.get('/', async (req, reply) => {
+  return 'hello world'
+})
+
+const interceptor = createFastifyInterceptor({
+  domain: '.local' // optional
+})
+
+const dispatcher = new Agent.compose(interceptor)
+dispatcher.route('myserver', server)
+
+request('http://myserver.local', {
+  dispatcher
+}).then((res) => {
+  return res.body.text()
+}).then(console.log)
+```
+
+## Usage as a Dispatcher
+
+```js
+const { request, Agent } = require('undici')
+const FastifyUndiciDispatcher = require('fastify-undici-dispatcher')
+const Fastify = require('fastify')
 const server = Fastify()
 server.get('/', async (req, reply) => {
   return 'hello world'
--- index.js
+++ index.js
@@ -2,83 +2,94 @@
 
 const { Dispatcher } = require('undici')
 
+function createFastifyInterceptor (opts) {
+  const routes = new Map()
+  const domain = opts?.domain
+  const res = (dispatch) => {
+    return (opts, handler) => {
+      let url = opts.origin
+      if (!(url instanceof URL)) {
+        url = new URL(opts.path, url)
+      }
+
+      const wrap = routes.get(url.hostname)
+      if (!wrap) {
+        if (dispatch && (domain === undefined || !url.hostname.endsWith(domain))) {
+          return dispatch(opts, handler)
+        } else {
+          throw new Error('No server found for ' + url.hostname)
+        }
+      }
+
+      const { server, address } = wrap
+
+      if (opts.headers) {
+        delete opts.headers.connection
+        delete opts.headers['transfer-encoding']
+      }
+
+      if (address) {
+        if (address.family === 'IPv6') {
+          opts.origin = `http://[${address.address}]:${address.port}`
+        } else {
+          opts.origin = `http://${address.address}:${address.port}`
+        }
+
+        return dispatch(opts, handler)
+      }
+
+      server.inject({
+        method: opts.method,
+        url: url.pathname + url.search,
+        headers: opts.headers,
+        query: opts.query,
+        body: opts.body
+      }).then(res => {
+        const headers = []
+        for (const [key, value] of Object.entries(res.headers)) {
+          if (Array.isArray(value)) {
+            for (const v of value) {
+              headers.push(key)
+              headers.push(v)
+            }
+          } else {
+            headers.push(key)
+            headers.push(value)
+          }
+        }
+        handler.onHeaders(res.statusCode, headers, () => {}, res.statusMessage)
+        handler.onData(res.rawPayload)
+        handler.onComplete([])
+        /* c8 ignore next 3 */
+      }).catch(err => {
+        handler.onError(err)
+      })
+      return true
+    }
+  }
+
+  res.route = (url, server) => {
+    if (domain && !url.endsWith(domain)) {
+      url += domain
+    }
+    const address = server.server.address()
+    routes.set(url, { server, address })
+  }
+
+  return res
+}
+
 class FastifyUndiciDispatcher extends Dispatcher {
   constructor (opts) {
     super()
     this.dispatcher = opts?.dispatcher
-    this.routes = new Map()
-    this._domain = opts?.domain
-  }
-
-  route (url, server) {
-    if (this._domain && !url.endsWith(this._domain)) {
-      url += this._domain
+    const interceptor = createFastifyInterceptor(opts)
+    this.route = interceptor.route
+    if (this.dispatcher) {
+      this.dispatch = interceptor(this.dispatcher.dispatch.bind(this.dispatcher))
+    } else {
+      this.dispatch = interceptor()
     }
-    const address = server.server.address()
-    this.routes.set(url, { server, address })
-  }
-
-  dispatch (opts, handler) {
-    let url = opts.origin
-    if (!(url instanceof URL)) {
-      url = new URL(opts.path, url)
-    }
-
-    const wrap = this.routes.get(url.hostname)
-    if (!wrap) {
-      if (this.dispatcher && (this._domain === undefined || !url.hostname.endsWith(this._domain))) {
-        return this.dispatcher.dispatch(opts, handler)
-      } else {
-        throw new Error('No server found for ' + url.hostname)
-      }
-    }
-
-    const { server, address } = wrap
-
-    if (opts.headers) {
-      delete opts.headers.connection
-      delete opts.headers['transfer-encoding']
-    }
-
-    if (address) {
-      if (address.family === 'IPv6') {
-        opts.origin = `http://[${address.address}]:${address.port}`
-      } else {
-        opts.origin = `http://${address.address}:${address.port}`
-      }
-
-      if (this.dispatcher) {
-        return this.dispatcher.dispatch(opts, handler)
-      }
-    }
-
-    server.inject({
-      method: opts.method,
-      url: url.pathname + url.search,
-      headers: opts.headers,
-      query: opts.query,
-      body: opts.body
-    }).then(res => {
-      const headers = []
-      for (const [key, value] of Object.entries(res.headers)) {
-        if (Array.isArray(value)) {
-          for (const v of value) {
-            headers.push(key)
-            headers.push(v)
-          }
-        } else {
-          headers.push(key)
-          headers.push(value)
-        }
-      }
-      handler.onHeaders(res.statusCode, headers, () => {}, res.statusMessage)
-      handler.onData(res.rawPayload)
-      handler.onComplete([])
-      /* c8 ignore next 3 */
-    }).catch(err => {
-      handler.onError(err)
-    })
-    return true
   }
 
   close () {
@@ -95,3 +106,4 @@ class FastifyUndiciDispatcher extends Dispatcher {
 }
 
 module.exports = FastifyUndiciDispatcher
+module.exports.createFastifyInterceptor = createFastifyInterceptor
